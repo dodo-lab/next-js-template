0ad5d8e1e12ad5148d954d952ed216770e8d2fe5
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-02-19 11:34:28 +0900

    ✨: トップページをmuiで表現

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,28 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import {createTheme, CssBaseline, ThemeProvider} from '@mui/material';
 import type {AppProps} from 'next/app';
-import 'styles/globals.css';
+
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1200,
+      xl: 1800,
+    },
+  },
+});
 
 function MyApp({Component, pageProps}: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
