3c33ca088fde78ecb66d9b278899d934a0158c2a
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-10-04 07:16:37 +0900

    ➕: eslint-plugin-strict-dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+function path(p) {
+  return [p, p.replace(/\//g, '\\')];
+}
+
+module.exports = {
+  extends: 'next/core-web-vitals',
+  plugins: ['strict-dependencies'],
+  rules: {
+    'strict-dependencies/strict-dependencies': [
+      'error',
+      [
+        {
+          module: 'framework/server',
+          allowReferenceFrom: [...path('src/pages/api')],
+        },
+      ],
+    ],
+  },
+};
